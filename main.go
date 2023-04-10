a8cf6caf47806357aabb7211ff68e2716bfd0377
andres06-hub <andressierrarojas7@gmail.com>
2023-04-10 00:32:22 -0500

    feat: new router, create task and get tasks

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -25,13 +27,44 @@ var tasks = allTask{
 }
 
 func homeRoute(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "welcome to my API/home")
+	// fmt.Fprintf(res, "welcome to my API/home ...")
+	json.NewEncoder(res).Encode("Welcome to my API/home")
+}
+
+func getTasks(res http.ResponseWriter, req *http.Request) {
+	fmt.Println("get tasks...")
+	//create JSON
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(tasks)
+}
+
+func createTask(res http.ResponseWriter, req *http.Request) {
+	var newTask task
+	//get body
+	reqBody, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		res.Write([]byte("Insert a valid task"))
+	}
+	//
+	json.Unmarshal(reqBody, &newTask)
+	//generated id
+	newTask.Id = len(tasks) + 1
+	//Add task created
+	tasks = append(tasks, newTask)
+	//Response
+	// headers and status code
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(http.StatusCreated)
+	json.NewEncoder(res).Encode(newTask)
 }
 
 func main() {
+	//Created router
 	router := mux.NewRouter().StrictSlash(true)
+	//Created routers
 	router.HandleFunc("/", homeRoute)
-
+	router.HandleFunc("/task", getTasks).Methods("GET")
+	router.HandleFunc("/task", createTask).Methods("POST")
 	fmt.Println("Initial server on port: :5051")
 	log.Fatal(http.ListenAndServe(":5051", router))
 }
